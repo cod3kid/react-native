73bf425ea5fe3efbf48f5fd6f3912b5898fbce1f
cod3kid <djsufail@gmail.com>
2021-09-03 23:18:27 +0530

    Flat List Completed

--- 2-Flat-List/App.js
+++ 2-Flat-List/App.js
@@ -1,39 +1,88 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+import Duolingo from "./app/assets/duolingo.svg";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Feather-Bold": require("./app/assets/fonts/feather_bold.ttf"),
+  });
+
   const languages = [
+    { name: "English", imagePath: require("./app/assets/great-britain.png") },
     { name: "Spanish", imagePath: require("./app/assets/spain.png") },
+    { name: "Italian", imagePath: require("./app/assets/italy.png") },
     { name: "French", imagePath: require("./app/assets/france.png") },
     { name: "German", imagePath: require("./app/assets/germany.png") },
+    { name: "Japanese", imagePath: require("./app/assets/japan.png") },
+    { name: "Hindi", imagePath: require("./app/assets/india.png") },
+    { name: "Russian", imagePath: require("./app/assets/russia.png") },
+    { name: "Hebrew", imagePath: require("./app/assets/israel.png") },
   ];
 
-  return (
-    <View style={styles.container}>
-      <Text>Language Learning App</Text>
-      <FlatList
-        data={languages}
-        keyExtractor={(language) => language.name.toString()}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.list}>
-              <Image
-                source={item.imagePath}
-                style={{ width: 50, height: 50 }}
-              />
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
-          );
-        }}
-      />
-    </View>
-  );
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#6ACD08"
+          translucent={false}
+        />
+        <View style={styles.header}>
+          <Duolingo width={40} height={40} />
+          <Text style={styles.headerText}>duolingo</Text>
+        </View>
+        <FlatList
+          data={languages}
+          keyExtractor={(language) => language.name.toString()}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.list}>
+                <Image
+                  source={item.imagePath}
+                  style={{ width: 50, height: 50 }}
+                />
+                <Text style={styles.text}>{item.name}</Text>
+              </View>
+            );
+          }}
+        />
+      </SafeAreaView>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  header: {
+    height: 60,
+    backgroundColor: "#6ACD08",
+    justifyContent: "center",
+    marginBottom: 8,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
+  headerText: {
+    marginLeft: 20,
+    fontSize: 24,
+    color: "white",
+    fontFamily: "Feather-Bold",
   },
   list: {
     flexDirection: "row",
@@ -43,5 +92,11 @@ const styles = StyleSheet.create({
   text: {
     marginLeft: 10,
     fontSize: 16,
+  },
+  separator: {
+    height: 1,
+    marginVertical: 10,
+    marginHorizontal: 12,
+    backgroundColor: "#B7C6D9",
   },
 });
