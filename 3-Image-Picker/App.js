23f044a70d69196521c4db5f9b2133313a18d474
cod3kid <djsufail@gmail.com>
2021-09-05 21:59:06 +0530

    Permissions for accessing Media Library and Opens Media Library on click

--- 3-Image-Picker/App.js
+++ 3-Image-Picker/App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!granted) alert("you need to give permission");
+  };
+
+  const selectImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={selectImage}>
+        <View style={styles.cameraContainer}>
+          <MaterialCommunityIcons name="camera" size={30} color="black" />
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -14,8 +36,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cameraContainer: {
+    height: 150,
+    width: 150,
+    backgroundColor: "#B7C6D9",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
   },
 });
