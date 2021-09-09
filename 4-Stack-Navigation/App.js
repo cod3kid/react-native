fd2e2439e2b8634b9656aea8ddc0185b8c44408c
cod3kid <djsufail@gmail.com>
2021-09-09 22:23:48 +0530

    Stack Navigation Added

--- 4-Stack-Navigation/App.js
+++ 4-Stack-Navigation/App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  const StackNavigator = () => {
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Screen1} />
+      <Stack.Screen name="Inside" component={Screen2} />
+    </Stack.Navigator>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
