11edbbbeb09bb7120b7b9139605dac68fe1e7fe5
cod3kid <djsufail@gmail.com>
2021-09-10 21:41:58 +0530

    Stack Navigation Complete

--- 4-Stack-Navigation/App.js
+++ 4-Stack-Navigation/App.js
@@ -3,15 +3,19 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Screen1 from "./Screen1";
+import Screen2 from "./Screen2";
 
 export default function App() {
   const Stack = createStackNavigator();
 
   const StackNavigator = () => {
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Screen1} />
-      <Stack.Screen name="Inside" component={Screen2} />
-    </Stack.Navigator>;
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Screen1} />
+        <Stack.Screen name="Inside" component={Screen2} />
+      </Stack.Navigator>
+    );
   };
 
   return (
--- 4-Stack-Navigation/Screen1.js
+++ 4-Stack-Navigation/Screen1.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Screen2 from "./Screen2";
 
-export default function Screen1() {
+export default function Screen1({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Screen One</Text>
+      <Button
+        title="Go to Screen 2"
+        onPress={() => navigation.push("Inside")}
+      />
     </View>
   );
 }
