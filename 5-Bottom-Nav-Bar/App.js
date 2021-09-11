2a6074afbe88098dec09a95420194efc980b4297
cod3kid <djsufail@gmail.com>
2021-09-11 16:55:45 +0530

    Simple Bottom Nav

--- 5-Bottom-Nav-Bar/App.js
+++ 5-Bottom-Nav-Bar/App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Home from "./Screens/Home";
+import Settings from "./Screens/Settings";
+import Profile from "./Screens/Profile";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
+  const TabNavigator = () => {
+    return (
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: () => (
+              <MaterialCommunityIcons name="wrench" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: () => (
+              <MaterialCommunityIcons name="home" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: () => (
+              <MaterialCommunityIcons name="account" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
