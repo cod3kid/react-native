5821138e175330f7d64c0c0d6df07d4ee51200aa
cod3kid <djsufail@gmail.com>
2021-09-14 20:26:08 +0530

    Modal Added

--- 6-Todo-App/App.js
+++ 6-Todo-App/App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Modal,
+  TouchableWithoutFeedback,
+  TextInput,
+} from "react-native";
 import FAB from "./Components/FAB";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
+  const [isModalVisible, setisModalVisible] = useState(false);
+  const addItem = () => {
+    setisModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
-      <FAB />
+      <View style={styles.fabContainer}>
+        <FAB style={styles.fab} onPress={addItem} />
+      </View>
+      <Modal visible={isModalVisible} animationType="slide" transparent>
+        <View style={styles.modal}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalHeaderText}>Add a Task</Text>
+            <TouchableWithoutFeedback onPress={() => setisModalVisible(false)}>
+              <MaterialCommunityIcons name="close" size={24} color="black" />
+            </TouchableWithoutFeedback>
+          </View>
+          <View>
+            <TextInput />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -15,9 +43,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
-    paddingBottom: 20,
-    paddingRight: 20,
+    alignItems: "center",
+  },
+  modal: {
+    backgroundColor: "gray",
+    marginHorizontal: 50,
+    marginTop: "50%",
+    height: 300,
+    borderRadius: 16,
+  },
+  modalHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  modalHeaderText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  fabContainer: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
   },
 });
--- 6-Todo-App/Components/FAB.js
+++ 6-Todo-App/Components/FAB.js
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text, TouchableWithoutFeedback, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function FAB() {
+export default function FAB({ onPress }) {
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.button}>
         <MaterialCommunityIcons name="plus" size={60} color="white" />
       </View>
@@ -15,10 +15,10 @@ export default function FAB() {
 const styles = StyleSheet.create({
   button: {
     backgroundColor: "blue",
-    height: 80,
-    width: 80,
+    height: 60,
+    width: 60,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 40,
+    borderRadius: 30,
   },
 });
