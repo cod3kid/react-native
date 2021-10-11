f23e866d03c43a78d1767c1c093e58887be82de9
cod3kid <djsufail@gmail.com>
2021-10-11 21:55:08 +0530

    New SignUp Screen

--- 7-Instagram-Clone/app/navigators/AuthNavigator.js
+++ 7-Instagram-Clone/app/navigators/AuthNavigator.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/AuthScreens/LoginScreen";
 import RegisterScreen from "../screens/AuthScreens/RegisterScreen";
-import SignUpScreen from "../screens/AuthScreens/SignUpScreen";
 
 const Stack = createStackNavigator();
 
@@ -16,11 +15,6 @@ const AuthNavigator = () => (
     <Stack.Screen
       name="Register"
       component={RegisterScreen}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen
-      name="SignUp"
-      component={SignUpScreen}
       options={{ headerShown: false }}
     />
   </Stack.Navigator>
--- 7-Instagram-Clone/app/screens/AuthScreens/RegisterScreen.js
+++ 7-Instagram-Clone/app/screens/AuthScreens/RegisterScreen.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import t from "../../utils/translations";
 import { colors } from "../../utils/colors";
 import Screen from "../../components/Common/Screen";
+import CustomInput from "../../components/Auth/CustomInput";
+import CustomButton from "../../components/Auth/CustomButton";
+import LanguageModal from "../../components/Auth/LanguageModal";
+import LanguageSelector from "../../components/Auth/LanguageSelector";
+import InstagramText from "../../components/Auth/InstagramText";
 import FBLoginButton from "../../components/Auth/FBLoginButton";
 import Footer from "../../components/Auth/Footer";
-import InstagramText from "../../components/Auth/InstagramText";
-import LanguageSelector from "../../components/Auth/LanguageSelector";
-import LanguageModal from "../../components/Auth/LanguageModal";
 import OrContainer from "../../components/Auth/OrContainer";
 
-export default function RegisterScreen({ navigation }) {
+export default function LoginScreen({ navigation }) {
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isModalVisible, setModalVisible] = useState(false);
 
   return (
@@ -19,17 +26,33 @@ export default function RegisterScreen({ navigation }) {
       <LanguageSelector onPress={() => setModalVisible(true)} />
       <View style={styles.mainContainer}>
         <InstagramText />
-        <View style={styles.topMargin}>
-          <FBLoginButton backgroundColor="#1778F2" color="white" />
-          <View style={styles.secondLastContainer}>
-            <OrContainer paddingHorizontal={10} />
-            <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
-              <Text style={styles.signWithEmailOrPhoneText}>
-                {t("signUpWithEmailOrPhone")}
-              </Text>
-            </TouchableOpacity>
-          </View>
+        <View style={styles.formContainer}>
+          <CustomInput
+            placeholder={t("fullName")}
+            value={fullName}
+            onChangeText={(text) => setFullName(text)}
+          />
+          <CustomInput
+            placeholder={t("email")}
+            value={userName}
+            onChangeText={(text) => setUserName(text)}
+          />
+          <CustomInput
+            placeholder={t("username")}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <CustomInput
+            placeholder={t("password")}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            isPassword
+            showIcon
+          />
+          <CustomButton title={t("signUpButton")} />
         </View>
+        <OrContainer paddingHorizontal={30} />
+        <FBLoginButton backgroundColor="white" color="#1778F2" />
       </View>
       <Footer
         text={t("preLogInText")}
@@ -50,24 +73,25 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   mainContainer: {
-    position: "absolute",
-    top: "30%",
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
     alignItems: "center",
-    alignSelf: "center",
-    width: "100%",
-    height: "40%",
-    padding: 20,
   },
-  topMargin: {
-    marginTop: "30%",
-  },
-  secondLastContainer: {
+  formContainer: {
     width: "100%",
+    padding: 30,
+  },
+  forgotPassContainer: {
+    padding: 5,
+    justifyContent: "center",
     alignItems: "center",
-    marginTop: 50,
   },
-  signWithEmailOrPhoneText: {
+  textContainer: {
+    textAlign: "center",
+  },
+  forgotLoginText: {
+    color: colors.grey,
+  },
+  getLoginHelpText: {
     color: colors.darkBlue,
     fontWeight: "bold",
   },
